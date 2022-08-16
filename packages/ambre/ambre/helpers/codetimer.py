a2c6f17b1673feea088b60425f47a42fcce5ab13
Timo Klimmer <timoklimmer@users.noreply.github.com>
2022-08-16 21:04:40 +0200

    Minor changes

---
+++ packages/ambre/ambre/helpers/codetimer.py
@@ -0,0 +1,24 @@
+"""Provides a context manager to time code blocks."""
+
+import pandas as pd
+import timeit
+
+class CodeTimer:
+    """Context manager to time a code block."""
+
+    def __init__(self, activity_name=None):
+        """Init."""
+        self.start = None
+        self.activity_name = activity_name
+
+    def __enter__(self):
+        """Start timing when context is entered."""
+        self.start = timeit.default_timer()
+        if self.activity_name:
+            print(f"{self.activity_name}...")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Exit timing when context is exited."""
+        ms_taken = (timeit.default_timer() - self.start) * 1000.0
+        pd_time_delta = pd.to_timedelta(ms_taken, unit="ms")
+        print(f"Time taken: {pd_time_delta}.")
---
+++ packages/ambre/ambre/helpers/strings.py
@@ -0,0 +1,41 @@
+"""Several common helper functions used when working with strings."""
+
+import string
+
+def compress_string(
+    string,
+    input_alphabet=string.printable,
+    output_alphabet="".join((chr(char_number) for char_number in range(0, 256))),
+):
+    """Compress the given string to a shorter string, using a longer output alphabet than the input alphabet."""
+    input_base = len(input_alphabet)
+    cumulated_number = 0
+    is_first_char = True
+    for char in string:
+        position_in_input_alphabet = input_alphabet.find(char)
+        if position_in_input_alphabet >= 0:
+            cumulated_number = cumulated_number * input_base + position_in_input_alphabet
+            if is_first_char and cumulated_number == 0:
+                cumulated_number = input_base
+        else:
+            raise ValueError(
+                f"Character '{char}' does not exist in input alphabet '{input_alphabet}'. Ensure that the string "
+                f"'{string}' has only valid characters from the input alphabet or extend the input alphabet."
+            )
+        is_first_char = False
+    result = ""
+    output_base = len(output_alphabet)
+    while cumulated_number > 0:
+        result = output_alphabet[cumulated_number % output_base] + result
+        cumulated_number //= output_base
+
+    return result
+
+
+def decompress_string(
+    compressed_string,
+    original_input_alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    original_output_alphabet="".join((chr(char_number) for char_number in range(0, 256))),
+):
+    """Decompress the given string to its original value."""
+    return compress_string(compressed_string, original_output_alphabet, original_input_alphabet)

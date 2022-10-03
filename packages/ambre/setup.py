c61ecde2659861c69810b77db95ec7ee3055f1b0
Timo Klimmer <timoklimmer@users.noreply.github.com>
2022-10-03 20:02:35 +0200

    Bugfix: Installation issues

--- packages/ambre/setup.py
+++ packages/ambre/setup.py
@@ -1,11 +1,29 @@
-"""Install script for the ambre package."""
+"""
+Install script for the package.
 
-from setuptools import setup, find_packages
-from ambre.versions import AMBRE_PACKAGE_VERSION
+Note: We need to load the versions.py script separately here because we cannot import it without loading the entire
+      package. When this script is run, the package is however not available yet. Hence need to import the versions
+      module separately.
+"""
+
+from importlib import util
+
+from setuptools import find_packages, setup
+
+
+def load_file_as_module(name, location):
+    """Load Python script as module."""
+    spec = util.spec_from_file_location(name, location)
+    module = util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+versions = load_file_as_module("versions", "ambre/versions.py")
 
 setup(
     name="ambre",
-    version=AMBRE_PACKAGE_VERSION,
+    version=versions.AMBRE_PACKAGE_VERSION,
     description="An Association Mining-Based Rules Extraction engine. Find the needle in the haystack.",
     url="https://github.com/timoklimmer/ambre",
     author="Timo Klimmer",

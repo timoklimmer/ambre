091cd2409aef1caa1dd56d63b9ee320aad89d57d
Timo Klimmer <25802428+timoklimmer@users.noreply.github.com>
2023-05-31 14:19:50 +0200

    Minor update

--- lex_powerset.py
+++ lex_powerset.py
@@ -1,18 +1,28 @@
 from typing import List
 
 def lex_powerset(lst: List[str]) -> List[List[str]]:
+    lst = list(set(lst))
     lst.sort(reverse=True)
-    powerset = []
     stack = [[]]
     while stack:
         subset = stack.pop()
         if subset:
-            powerset.append(subset)
             yield(subset)
         for i in range(len(lst)):
             if not subset or lst[i] > subset[-1]:
                 new_subset = subset + [lst[i]]
                 stack.append(new_subset)
 
-for i in lex_powerset(["a", "b", "c"]):
+items = ["a", "b", "c", "d", "e"]
+
+counter = 0
+for i in lex_powerset(items):
     print(i)
+    counter += 1
+
+print(counter)
+
+expected_counter = 2 ** len(items) - 1
+print(expected_counter)
+
+assert(counter == expected_counter)
